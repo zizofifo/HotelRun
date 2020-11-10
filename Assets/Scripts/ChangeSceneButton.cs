513d627f3b46d7301e61ab1e5e2d99fef6d4e632
Cole Howard <zizofifo@tutanota.com>
2020-11-09 22:12:27 -0600

    Add title screen, a button script, and game logo

---
+++ Assets/Scripts/ChangeSceneButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneButton : MonoBehaviour
+{
+    [SerializeField]
+    public string targetSceneName;
+    
+    public void OnClicked()
+    {
+        // Catch possible Null Exception or let Unity move on?
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
