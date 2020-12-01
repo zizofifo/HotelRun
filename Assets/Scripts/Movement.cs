zizofifo/HotelRun

commit 427ac6e0f2d3eda1ece62b3706369ee0d6dfed35

Changes made during the 2020-11-20 Game Development Group meeting
This project ended up being the subject for most of Friday's Game
Development Group meeting. During that meeting, I was taught more about
rigidbodies and colliders (along with many things about tilemaps during
the earlier subject that meet), making changes to the project myself as
the meeting went along. I am considering applying these changes to the
whole game, rather than the small subset it was done to during the
meet.

Here is a list of (hopefully all) the changes in no particular order.
"The scene" refers to Stage01Scene:
* Take the overrides made to the PlayerSprite in the scene and apply
  them to the PlayerSprite prefab. While this was done to save the
  PlayerSprite's components before messing with them, these overrides
  should have been in the PlayerSprite anyway.
* Replace the Rigidbody on the first LuggageCart in the scene with a
  Ridgidbody2D.
* Disable all colliders inherited from the LuggageCart prefab on the
  first LuggageCart.
* Add a PolygonCollider2D, CircleCollider2D, and CompositeCollider2D to
  the first LuggageCart.
* Alter the first LuggageCart's Polygon2DCollider's polygon to better
  fit the the luggage cart's shape.
* Replace the Rigidbody on the PlayerSprite in the scene with a
  Rigidbody2D.
* Alter some properties of the Rigidbody2D used by the PlayerSprite in
  the scene.
* Replace the BoxCollider on the PlayerSprite in the scene with a
  PolygonCollider2D.
* Alter the PlayerSprite's PolygonCollider2D's polygon to better fit the
  PlayerSprite's shape (the initial, perfectly-fitting shape was
  automatically generated).
* Change the Floor to use a Rigidbody2D.
* Convert Movement.cs to use 2D versions of collision and trigger
  callbacks (and subsequently the types of their parameters), and
  rigidbody.
* Assign velocity when moving the player instead of assigning a
  position.

Thanks to Zach Martychenko, who led in teaching me, and to "AJ", who got

Assets/Scripts/Movement.cs
@@ -23,7 +23,7 @@ public class Movement : MonoBehaviour
     private bool isJumping = false;
     [SerializeField]
     private bool canJump = true;
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     private Animator anim;
 
     void Awake()
@@ -39,7 +39,7 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -62,24 +62,25 @@ public class Movement : MonoBehaviour
     void MovePlayer()
     {
         Vector2 position = transform.position;
+        Vector2 velocity = rb.velocity;
 
         float xMovement = Input.GetAxis("Horizontal");
         float yMovement = Input.GetAxis("Vertical");
         bool spacePressed = Input.GetKey(KeyCode.Space);
 
-        position.x += xMovement * speedMultiplier * Time.deltaTime;
+        velocity.x += xMovement * speedMultiplier * Time.deltaTime;
 
-        transform.position = position;
+        rb.velocity = velocity;
 
         if (spacePressed && !isJumping && canJump)
         {
             isJumping = true;
             canJump = false;
-            rb.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
+            rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
         }
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Player: Enter Collision with " + other.gameObject.tag);
 
@@ -95,7 +96,7 @@ public class Movement : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Player: Enter Trigger with " + other.gameObject.tag);
 
@@ -114,7 +115,7 @@ public class Movement : MonoBehaviour
         }
     }
 
-    void OnCollisionExit(Collision other)
+    void OnCollisionExit2D(Collision2D other)
     {
         Debug.Log("Player: Exit Collision with " + other.gameObject.tag);
 
@@ -125,7 +126,7 @@ public class Movement : MonoBehaviour
         */
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit2D(Collider2D other)
     {
         Debug.Log("Player: Exit Trigger with " + other.gameObject.tag);
 

Assets/Prefabs/PlayerSprite.prefab
@@ -11,7 +11,10 @@ GameObject:
   - component: {fileID: 4175284763666162004}
   - component: {fileID: 4280345155048666145}
   - component: {fileID: -331770523069258849}
-  m_Layer: 0
+  - component: {fileID: 1155615226}
+  - component: {fileID: 1155615227}
+  - component: {fileID: 1155615225}
+  m_Layer: 8
   m_Name: PlayerSprite
   m_TagString: Player
   m_Icon: {fileID: 0}
@@ -102,3 +105,53 @@ Animator:
   m_HasTransformHierarchy: 1
   m_AllowConstantClipSamplingOptimization: 1
   m_KeepAnimatorControllerStateOnDisable: 0
+--- !u!54 &1155615226
+Rigidbody:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1620019929901659830}
+  serializedVersion: 2
+  m_Mass: 1
+  m_Drag: 0
+  m_AngularDrag: 0.05
+  m_UseGravity: 1
+  m_IsKinematic: 0
+  m_Interpolate: 0
+  m_Constraints: 120
+  m_CollisionDetection: 1
+--- !u!65 &1155615227
+BoxCollider:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1620019929901659830}
+  m_Material: {fileID: 0}
+  m_IsTrigger: 0
+  m_Enabled: 1
+  serializedVersion: 2
+  m_Size: {x: 1, y: 2.92, z: 0.2}
+  m_Center: {x: 0, y: 0, z: 0}
+--- !u!114 &1155615225
+MonoBehaviour:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1620019929901659830}
+  m_Enabled: 1
+  m_EditorHideFlags: 0
+  m_Script: {fileID: 11500000, guid: 70668f77c8444094e9ef522aac490e3a, type: 3}
+  m_Name: 
+  m_EditorClassIdentifier: 
+  PlayerSprite: {fileID: 1620019929901659830}
+  isPlayerControlled: 1
+  speedMultiplier: 10
+  jumpHeight: 10
+  isMotivated: 0
+  inCrowd: 0
+  isStunned: 0
+  isJumping: 0
+  canJump: 1

Assets/Scenes/Stage01Scene.unity
@@ -174,11 +174,44 @@ PrefabInstance:
       propertyPath: m_LocalEulerAnglesHint.z
       value: 0
       objectReference: {fileID: 0}
+    - target: {fileID: 1163164233520088580, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
+    - target: {fileID: 1324872460531290560, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
+    - target: {fileID: 1397952209019817469, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
+    - target: {fileID: 2537502467621309854, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
+    - target: {fileID: 2707350565748727454, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
+    - target: {fileID: 3277922934514906926, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
+    - target: {fileID: 4493056115350078467, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
+    - target: {fileID: 8493247546859127948, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+      propertyPath: m_Enabled
+      value: 0
+      objectReference: {fileID: 0}
     - target: {fileID: 8551202732130149855, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
       propertyPath: m_Name
       value: LuggageCart
       objectReference: {fileID: 0}
-    m_RemovedComponents: []
+    m_RemovedComponents:
+    - {fileID: 5196585186301282845, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
   m_SourcePrefab: {fileID: 100100000, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
 --- !u!1001 &188467461
 PrefabInstance:
@@ -829,14 +862,6 @@ PrefabInstance:
       propertyPath: m_Name
       value: PlayerSprite
       objectReference: {fileID: 0}
-    - target: {fileID: 1620019929901659830, guid: aadfb0a1c3805524f805974fcb74d7c9, type: 3}
-      propertyPath: m_TagString
-      value: Player
-      objectReference: {fileID: 0}
-    - target: {fileID: 1620019929901659830, guid: aadfb0a1c3805524f805974fcb74d7c9, type: 3}
-      propertyPath: m_Layer
-      value: 8
-      objectReference: {fileID: 0}
     - target: {fileID: 4175284763666162004, guid: aadfb0a1c3805524f805974fcb74d7c9, type: 3}
       propertyPath: m_LocalPosition.x
       value: -145
@@ -881,7 +906,9 @@ PrefabInstance:
       propertyPath: m_LocalEulerAnglesHint.z
       value: 0
       objectReference: {fileID: 0}
-    m_RemovedComponents: []
+    m_RemovedComponents:
+    - {fileID: 1155615227, guid: aadfb0a1c3805524f805974fcb74d7c9, type: 3}
+    - {fileID: 1155615226, guid: aadfb0a1c3805524f805974fcb74d7c9, type: 3}
   m_SourcePrefab: {fileID: 100100000, guid: aadfb0a1c3805524f805974fcb74d7c9, type: 3}
 --- !u!1001 &848490867
 PrefabInstance:
@@ -1018,56 +1045,118 @@ GameObject:
   m_CorrespondingSourceObject: {fileID: 1620019929901659830, guid: aadfb0a1c3805524f805974fcb74d7c9, type: 3}
   m_PrefabInstance: {fileID: 801707228}
   m_PrefabAsset: {fileID: 0}
---- !u!114 &1155615225
-MonoBehaviour:
+--- !u!60 &1155615226
+PolygonCollider2D:
   m_ObjectHideFlags: 0
   m_CorrespondingSourceObject: {fileID: 0}
   m_PrefabInstance: {fileID: 0}
   m_PrefabAsset: {fileID: 0}
   m_GameObject: {fileID: 1155615222}
   m_Enabled: 1
-  m_EditorHideFlags: 0
-  m_Script: {fileID: 11500000, guid: 70668f77c8444094e9ef522aac490e3a, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  PlayerSprite: {fileID: 1155615222}
-  isPlayerControlled: 1
-  speedMultiplier: 10
-  jumpHeight: 10
-  isMotivated: 0
-  inCrowd: 0
-  isStunned: 0
-  isJumping: 0
-  canJump: 1
---- !u!54 &1155615226
-Rigidbody:
-  m_ObjectHideFlags: 0
-  m_CorrespondingSourceObject: {fileID: 0}
-  m_PrefabInstance: {fileID: 0}
-  m_PrefabAsset: {fileID: 0}
-  m_GameObject: {fileID: 1155615222}
-  serializedVersion: 2
-  m_Mass: 1
-  m_Drag: 0
-  m_AngularDrag: 0.05
-  m_UseGravity: 1
-  m_IsKinematic: 0
-  m_Interpolate: 0
-  m_Constraints: 120
-  m_CollisionDetection: 1
---- !u!65 &1155615227
-BoxCollider:
-  m_ObjectHideFlags: 0
-  m_CorrespondingSourceObject: {fileID: 0}
-  m_PrefabInstance: {fileID: 0}
-  m_PrefabAsset: {fileID: 0}
-  m_GameObject: {fileID: 1155615222}
+  m_Density: 1
   m_Material: {fileID: 0}
   m_IsTrigger: 0
-  m_Enabled: 1
-  serializedVersion: 2
-  m_Size: {x: 1, y: 2.92, z: 0.2}
-  m_Center: {x: 0, y: 0, z: 0}
+  m_UsedByEffector: 0
+  m_UsedByComposite: 0
+  m_Offset: {x: 0, y: 0}
+  m_SpriteTilingProperty:
+    border: {x: 0, y: 0, z: 0, w: 0}
+    pivot: {x: 0.5, y: 0.5}
+    oldSize: {x: 1, y: 2.92}
+    newSize: {x: 1, y: 2.92}
+    adaptiveTilingThreshold: 0.5
+    drawMode: 0
+    adaptiveTiling: 0
+  m_AutoTiling: 0
+  m_Points:
+    m_Paths:
+    - - {x: -0.03, y: -0.76}
+      - {x: -0.01, y: -0.7}
+      - {x: 0.01, y: -0.77}
+      - {x: 0.099999994, y: -1}
+      - {x: 0.26, y: -1.41}
+      - {x: 0.29999998, y: -1.4599999}
+      - {x: 0.32999998, y: -1.4599999}
+      - {x: 0.37, y: -1.4399999}
+      - {x: 0.32999998, y: -1.28}
+      - {x: 0.25, y: -1.1}
+      - {x: 0.16, y: -0.87}
+      - {x: 0.07, y: -0.65}
+      - {x: 0.03, y: -0.57}
+      - {x: 0.04, y: -0.07}
+      - {x: 0.099999994, y: -0.16}
+      - {x: 0.32999998, y: -0.48999998}
+      - {x: 0.45, y: -0.65999997}
+      - {x: 0.5, y: -0.65}
+      - {x: 0.5, y: -0.55}
+      - {x: 0.45999998, y: -0.48999998}
+      - {x: 0.14, y: -0.03}
+      - {x: 0.03, y: 0.11}
+      - {x: 0.06, y: 0.53999996}
+      - {x: 0.11, y: 0.55}
+      - {x: 0.19, y: 0.57}
+      - {x: 0.28, y: 0.61}
+      - {x: 0.39999998, y: 0.74}
+      - {x: 0.45999998, y: 0.85999995}
+      - {x: 0.48999998, y: 0.95}
+      - {x: 0.48, y: 1.13}
+      - {x: 0.45999998, y: 1.1899999}
+      - {x: 0.37, y: 1.3299999}
+      - {x: 0.28, y: 1.41}
+      - {x: 0.14999999, y: 1.4599999}
+      - {x: -0.19, y: 1.4599999}
+      - {x: -0.29999998, y: 1.39}
+      - {x: -0.35999998, y: 1.3399999}
+      - {x: -0.45, y: 1.23}
+      - {x: -0.48999998, y: 1.12}
+      - {x: -0.5, y: 1.0699999}
+      - {x: -0.48999998, y: 0.89}
+      - {x: -0.47, y: 0.83}
+      - {x: -0.38, y: 0.69}
+      - {x: -0.29, y: 0.62}
+      - {x: -0.19999999, y: 0.56}
+      - {x: -0.06, y: 0.53}
+      - {x: -0.06, y: 0.12}
+      - {x: -0.099999994, y: 0.07}
+      - {x: -0.28, y: -0.17999999}
+      - {x: -0.45999998, y: -0.44}
+      - {x: -0.5, y: -0.51}
+      - {x: -0.5, y: -0.59999996}
+      - {x: -0.45, y: -0.62}
+      - {x: -0.24, y: -0.31}
+      - {x: -0.13, y: -0.14999999}
+      - {x: -0.06, y: -0.58}
+      - {x: -0.17, y: -0.81}
+      - {x: -0.24, y: -0.98999995}
+      - {x: -0.37, y: -1.28}
+      - {x: -0.45, y: -1.4599999}
+      - {x: -0.34, y: -1.4599999}
+      - {x: -0.26, y: -1.31}
+      - {x: -0.14999999, y: -1.03}
+    - - {x: -0.12, y: 0.65999997}
+      - {x: -0.14, y: 0.65999997}
+      - {x: -0.07, y: 0.65}
+--- !u!50 &1155615227
+Rigidbody2D:
+  serializedVersion: 4
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1155615222}
+  m_BodyType: 0
+  m_Simulated: 1
+  m_UseFullKinematicContacts: 0
+  m_UseAutoMass: 0
+  m_Mass: 1
+  m_LinearDrag: 0
+  m_AngularDrag: 3.54
+  m_GravityScale: 1
+  m_Material: {fileID: 0}
+  m_Interpolate: 0
+  m_SleepingMode: 1
+  m_CollisionDetection: 0
+  m_Constraints: 4
 --- !u!1001 &1262330917
 PrefabInstance:
   m_ObjectHideFlags: 0
@@ -1139,7 +1228,9 @@ PrefabInstance:
       propertyPath: m_LocalScale.z
       value: 4
       objectReference: {fileID: 0}
-    m_RemovedComponents: []
+    m_RemovedComponents:
+    - {fileID: 2414259684249527670, guid: 903f8b1213355f042bdfb482e39a2b5e, type: 3}
+    - {fileID: 4741257169027865915, guid: 903f8b1213355f042bdfb482e39a2b5e, type: 3}
   m_SourcePrefab: {fileID: 100100000, guid: 903f8b1213355f042bdfb482e39a2b5e, type: 3}
 --- !u!1001 &1425093063
 PrefabInstance:
@@ -1494,6 +1585,153 @@ MonoBehaviour:
   m_Name: 
   m_EditorClassIdentifier: 
   camZ: 0
+--- !u!1 &1635959995 stripped
+GameObject:
+  m_CorrespondingSourceObject: {fileID: 8551202732130149855, guid: 375948ff40a49334481ad8cdbfb2994a, type: 3}
+  m_PrefabInstance: {fileID: 170006565}
+  m_PrefabAsset: {fileID: 0}
+--- !u!58 &1635959996
+CircleCollider2D:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1635959995}
+  m_Enabled: 1
+  m_Density: 1
+  m_Material: {fileID: 0}
+  m_IsTrigger: 0
+  m_UsedByEffector: 0
+  m_UsedByComposite: 0
+  m_Offset: {x: 0, y: 0}
+  serializedVersion: 2
+  m_Radius: 0.5
+--- !u!66 &1635959998
+CompositeCollider2D:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1635959995}
+  m_Enabled: 1
+  m_Density: 1
+  m_Material: {fileID: 0}
+  m_IsTrigger: 0
+  m_UsedByEffector: 0
+  m_UsedByComposite: 0
+  m_Offset: {x: 0, y: 0}
+  m_GeometryType: 0
+  m_GenerationType: 0
+  m_EdgeRadius: 0
+  m_ColliderPaths:
+  - m_Collider: {fileID: 1635959999}
+    m_ColliderPaths:
+    - - X: -40279160
+        Y: 26705586
+      - X: -40289156
+        Y: 20072294
+      - X: -45184172
+        Y: 19730258
+      - X: -45255660
+        Y: 10044003
+      - X: -40431596
+        Y: 9568913
+      - X: -39778204
+        Y: -2348864
+      - X: -3897413
+        Y: -2544978
+      - X: 5217682
+        Y: -2677009
+      - X: 40036696
+        Y: -2314620
+      - X: 39742584
+        Y: 10333986
+      - X: 45003356
+        Y: 10691211
+      - X: 44897688
+        Y: 20047694
+      - X: 40056532
+        Y: 20006260
+      - X: 39539328
+        Y: 27249806
+  m_CompositePaths:
+    m_Paths:
+    - - {x: 0.52176803, y: -0.2677009}
+      - {x: 4.003669, y: -0.2314322}
+      - {x: 3.974286, y: 1.0334005}
+      - {x: 4.500335, y: 1.0691487}
+      - {x: 4.4897394, y: 2.0047693}
+      - {x: 4.0056515, y: 2.000653}
+      - {x: 3.9539056, y: 2.7249804}
+      - {x: -4.027916, y: 2.6705296}
+      - {x: -4.0289426, y: 2.0072277}
+      - {x: -4.518418, y: 1.9729992}
+      - {x: -4.52554, y: 1.0043976}
+      - {x: -4.0431585, y: 0.9568669}
+      - {x: -3.9777932, y: -0.2348866}
+      - {x: -0.3897415, y: -0.2544978}
+  m_VertexDistance: 0.0005
+  m_OffsetDistance: 0.00005
+--- !u!60 &1635959999
+PolygonCollider2D:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1635959995}
+  m_Enabled: 1
+  m_Density: 1
+  m_Material: {fileID: 0}
+  m_IsTrigger: 0
+  m_UsedByEffector: 0
+  m_UsedByComposite: 1
+  m_Offset: {x: 0, y: 0}
+  m_SpriteTilingProperty:
+    border: {x: 0, y: 0, z: 0, w: 0}
+    pivot: {x: 0, y: 0}
+    oldSize: {x: 0, y: 0}
+    newSize: {x: 0, y: 0}
+    adaptiveTilingThreshold: 0
+    drawMode: 0
+    adaptiveTiling: 0
+  m_AutoTiling: 0
+  m_Points:
+    m_Paths:
+    - - {x: -4.027916, y: 2.6705587}
+      - {x: -4.0289154, y: 2.0072293}
+      - {x: -4.5184174, y: 1.9730258}
+      - {x: -4.525566, y: 1.0044003}
+      - {x: -4.0431595, y: 0.9568913}
+      - {x: -3.9778204, y: -0.23488641}
+      - {x: -0.38974136, y: -0.2544979}
+      - {x: 0.5217682, y: -0.2677009}
+      - {x: 4.0036697, y: -0.231462}
+      - {x: 3.9742584, y: 1.0333986}
+      - {x: 4.5003357, y: 1.0691211}
+      - {x: 4.489769, y: 2.0047693}
+      - {x: 4.0056534, y: 2.000626}
+      - {x: 3.9539328, y: 2.7249806}
+--- !u!50 &1635960000
+Rigidbody2D:
+  serializedVersion: 4
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1635959995}
+  m_BodyType: 0
+  m_Simulated: 1
+  m_UseFullKinematicContacts: 0
+  m_UseAutoMass: 0
+  m_Mass: 1
+  m_LinearDrag: 0
+  m_AngularDrag: 0.05
+  m_GravityScale: 1
+  m_Material: {fileID: 0}
+  m_Interpolate: 0
+  m_SleepingMode: 1
+  m_CollisionDetection: 0
+  m_Constraints: 4
 --- !u!1001 &1670960557
 PrefabInstance:
   m_ObjectHideFlags: 0
@@ -1504,6 +1742,10 @@ PrefabInstance:
     - target: {fileID: 278273361458159942, guid: 7150b8c77fc14bf4e90d6bcb376ca748, type: 3}
       propertyPath: m_LocalPosition.y
       value: 0.5
+      objectReference: {fileID: 0}
+    - target: {fileID: 1569181404730629482, guid: 7150b8c77fc14bf4e90d6bcb376ca748, type: 3}
+      propertyPath: m_Enabled
+      value: 1
       objectReference: {fileID: 0}
     - target: {fileID: 4830447592382523969, guid: 7150b8c77fc14bf4e90d6bcb376ca748, type: 3}
       propertyPath: m_LocalPosition.x
@@ -1563,6 +1805,58 @@ PrefabInstance:
       objectReference: {fileID: 0}
     m_RemovedComponents: []
   m_SourcePrefab: {fileID: 100100000, guid: 7150b8c77fc14bf4e90d6bcb376ca748, type: 3}
+--- !u!1 &1704078631 stripped
+GameObject:
+  m_CorrespondingSourceObject: {fileID: 4741257169027865912, guid: 903f8b1213355f042bdfb482e39a2b5e, type: 3}
+  m_PrefabInstance: {fileID: 1262330917}
+  m_PrefabAsset: {fileID: 0}
+--- !u!61 &1704078632
+BoxCollider2D:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1704078631}
+  m_Enabled: 1
+  m_Density: 1
+  m_Material: {fileID: 0}
+  m_IsTrigger: 0
+  m_UsedByEffector: 0
+  m_UsedByComposite: 0
+  m_Offset: {x: 0, y: 0}
+  m_SpriteTilingProperty:
+    border: {x: 0, y: 0, z: 0, w: 0}
+    pivot: {x: 0, y: 0}
+    oldSize: {x: 0, y: 0}
+    newSize: {x: 0, y: 0}
+    adaptiveTilingThreshold: 0
+    drawMode: 0
+    adaptiveTiling: 0
+  m_AutoTiling: 0
+  serializedVersion: 2
+  m_Size: {x: 0.99999994, y: 1}
+  m_EdgeRadius: 0
+--- !u!50 &1704078633
+Rigidbody2D:
+  serializedVersion: 4
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1704078631}
+  m_BodyType: 1
+  m_Simulated: 1
+  m_UseFullKinematicContacts: 0
+  m_UseAutoMass: 0
+  m_Mass: 1
+  m_LinearDrag: 0
+  m_AngularDrag: 0.05
+  m_GravityScale: 0
+  m_Material: {fileID: 0}
+  m_Interpolate: 0
+  m_SleepingMode: 1
+  m_CollisionDetection: 0
+  m_Constraints: 3
 --- !u!1001 &1716060162
 PrefabInstance:
   m_ObjectHideFlags: 0

commit b874cec9787a16461121a38c66f91d11bc0026dd

Halt PlayerSprite movement when he slips/stunned
This fixes a regression that was probably introduced in 427ac6e.

Assets/Scripts/Movement.cs
@@ -91,6 +91,11 @@ public class Movement : MonoBehaviour
             velocity.x = reverseCap;
         }
 
+        if (isStunned)
+        {
+            velocity = Vector2.zero;
+        }
+
         rb.velocity = velocity;
 
       