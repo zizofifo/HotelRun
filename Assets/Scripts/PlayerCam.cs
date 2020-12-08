c8a6a9dce7fd426fdb53e92a5b16b77a2a323cc4
Cole Howard <zizofifo@tutanota.com>
2020-12-08 15:10:12 -0600

    Turn the HUD into its own Prefab; refactor HUD
    The HUD is now completely separated from the PlayerCam. Now the same HUD
    can be used for multiple Stages (which are implemented as scenes).
    
    Just <s>add water</s> tell the HUD which PlayerSprite to use.

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -9,19 +9,9 @@ public class PlayerCam : MonoBehaviour
 {
     [Header("Set in inspector")]
     public GameObject POI;
-    public Movement player;
-    public TextMeshProUGUI uitTimer; //Timer text
-    public TextMeshProUGUI tmpSodas;
-    public Keycap sodaKeyCap;
-    public TextMeshProUGUI tmpTowels;
-    public Keycap towelsKeyCap;
 
     [Header("Set dynamically")]
     public float camZ;
-    public float timer = 0.0f;
-    public int seconds;
-    public int minutes;
-
 
     void Start()
     {
@@ -38,29 +28,5 @@ public class PlayerCam : MonoBehaviour
         pos.z = camZ;
 
         transform.position = pos;
-
-        timer += Time.deltaTime;
-
-        seconds = (int)(timer % 60);
-        minutes = (int)(timer / 60);
-
-        UpdateGUI();
-    }
-
-    void UpdateGUI()
-    {
-        string timerText = string.Format("{0:0}:{1:00}", minutes, seconds);
-
-        tmpSodas.text = "Sodas:   " + string.Format("{0:#0}", player.sodaCans);
-
-        sodaKeyCap.SetAppearance(player.isMotivated ?
-            Keycap.KeycapAppearance.PleaseWait :
-            !player.isMotivated && player.sodaCans > 0 ? Keycap.KeycapAppearance.Enabled : Keycap.KeycapAppearance.Disabled);
-
-        towelsKeyCap.SetAppearance(player.isUsingTowel ?
-            Keycap.KeycapAppearance.PleaseWait :
-            !player.isUsingTowel && player.towels > 0 ? Keycap.KeycapAppearance.Enabled : Keycap.KeycapAppearance.Disabled);
-
-        uitTimer.text = "Time: "+timerText;
     }
 }
