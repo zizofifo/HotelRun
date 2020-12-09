3ccb413294d51e6e71384abb07a1f79fed62300b
Reed <reedcollins33@gmail.com>
2020-12-08 20:02:33 -0600

    Revert "Edited Instructions and beginnings of time high scores"
    This reverts commit 546a18e86bbe434b6b5f21d3bb3bb866e59bdeae.

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -9,12 +9,10 @@ public class PlayerCam : MonoBehaviour
 
     [Header("Set in inspector")]
     public Text uitTimer; //Timer text
-    public Text uitWorldRecord; //Best time recorded
 
     [Header("Set dynamically")]
     public float camZ;
     public float timer = 0.0f;
-    public float worldRecord = 1000.0f;
     public int seconds;
     public int minutes;
 
@@ -48,9 +46,5 @@ public class PlayerCam : MonoBehaviour
         string timerText = string.Format("{0:0}:{1:00}", minutes, seconds);
 
         uitTimer.text = "Time: "+timerText;
-
-        string recordText = string.Format("{0:0}:{1:00}", minutes, seconds);
-
-        uitWorldRecord.text = "Time: "+recordText;
     }
 }
