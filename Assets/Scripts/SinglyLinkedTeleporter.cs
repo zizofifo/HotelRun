e3cfa25db4cd0fdf140c753350faf538e816837b
Cole Howard <zizofifo@tutanota.com>
2020-12-10 22:22:41 -0600

    Begin desperate effort to get Rival teleporting to work better
    Using my own code instead of Harrison merely because I understand my
    code better. That is not supposed to be a jab or anything at all,
    truly.

--- Assets/Scripts/SinglyLinkedTeleporter.cs
+++ Assets/Scripts/SinglyLinkedTeleporter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stairwell : MonoBehaviour
+public class SinglyLinkedTeleporter : MonoBehaviour
 {
     public GameObject destination;
 
